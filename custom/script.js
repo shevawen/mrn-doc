5ffbdbbbee7a9f6ad6b2ba346807287da948ef64
binggg <being99@qq.com>
2015-11-24 16:36:34 +0800

    Update

--- custom/script.js
+++ custom/script.js
@@ -1,9 +1,9 @@
 window.onload = function () {
-    var phone = document.createElement('element');
+    var phone = document.createElement('iframe');
 
-    phone.setAttribute("src","https://appetize.io/embed/j48zj9r83cetpd1mhg4g8buc4w?device=nexus5&scale=100&autoplay=true&orientation=portrait&deviceColor=black");
-    phone.setAttribute("width","400px");
-    phone.setAttribute("height","795px");
+    phone.setAttribute("src","https://appetize.io/embed/j48zj9r83cetpd1mhg4g8buc4w?device=nexus5&scale=75&autoplay=true&orientation=portrait&deviceColor=black");
+    phone.setAttribute("width","300px");
+    phone.setAttribute("height","597px");
     phone.setAttribute("frameborder","0");
     phone.setAttribute("scrolling","no");
     phone.setAttribute("class","phone");
