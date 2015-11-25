606bdfd4638929eaa3e65f39ba8f51fe63c54b41
binggg <being99@qq.com>
2015-11-25 19:39:37 +0800

    Update

--- user/script/0-script.js
+++ user/script/0-script.js
@@ -1,12 +1,16 @@
 window.onload = function () {
+    var phoneContainer = document.createElement('div');
     var phone = document.createElement('iframe');
 
+    phoneContainer.setAttribute("class","phone");
+
+    phone.setAttribute("frameborder","0");
     phone.setAttribute("src","https://appetize.io/embed/j48zj9r83cetpd1mhg4g8buc4w?device=nexus5&scale=75&autoplay=true&orientation=portrait&deviceColor=black");
     phone.setAttribute("width","300px");
     phone.setAttribute("height","597px");
-    phone.setAttribute("frameborder","0");
     phone.setAttribute("scrolling","no");
-    phone.setAttribute("class","phone");
 
-    document.querySelector('.content').appendChild(phone);
+    phoneContainer.appendChild(phone);
+
+    document.querySelector('.content').appendChild(phoneContainer);
 };
